1a4034709426384440cddc80da315cf0f41f7d44
Nnamdi <nnamdi.b@gmail.com>
2022-07-11 21:33:41 +0200

    finished styling the mobile menu

--- components/navbar.js
+++ components/navbar.js
@@ -2,8 +2,13 @@ import styles from './navbar.module.scss';
 import Image from 'next/image';
 import images from './images';
 import Link from 'next/link';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className={styles.app__navbar}>
       <div className={styles.app__navbar_logo}>
@@ -17,6 +22,25 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
+      <div className={styles.app__navbar_menu}>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <Link href={`/${item}`}>{item}</Link>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 }
